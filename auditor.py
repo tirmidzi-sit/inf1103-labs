036ceddea845626f0cd630da7e6e10d9777f75ae
Tirmidzi <tirmidzisallehin1@gmail.com>
2026-09-15 11:50:23 +0800

    Add integer stock input

--- auditor.py
+++ auditor.py
@@ -3,4 +3,11 @@
 inventory = 0
 
 while True:
-    stock = input("Enter stock quantity: ")
+    stock = input("Enter stock quantity or 'quit': ")
+
+    if stock.lower() == "quit":
+        break
+
+    quantity = int(stock)
+
+    print("Stock quantity:", quantity)
