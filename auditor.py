1ec07eff0cb403614b2102fe19798528f8bf268a
Tirmidzi <tirmidzisallehin1@gmail.com>
2026-09-15 12:10:32 +0800

    Count rejected entries

--- auditor.py
+++ auditor.py
@@ -1,6 +1,7 @@
 # Inventory Auditor
 
 inventory = 0
+failed_entries = 0
 
 while True:
     stock = input("Enter stock quantity or 'quit': ")
@@ -10,6 +11,7 @@ while True:
 
     elif stock.startswith("-") and stock[1:].isdigit():
         print("Error: Negative numbers are not allowed.")
+        failed_entries += 1
 
     elif stock.isdigit():
         quantity = int(stock)
@@ -22,3 +24,4 @@ while True:
         
     else:
         print("Error: Invalid input.")
+        failed_entries += 1
