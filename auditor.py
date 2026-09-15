984ab39322fff55d3b9eddb70808ec99b2296160
Tirmidzi <tirmidzisallehin1@gmail.com>
2026-09-15 11:56:38 +0800

    Reject negative numbers

--- auditor.py
+++ auditor.py
@@ -8,6 +8,9 @@ while True:
     if stock.lower() == "quit":
         break
 
+    elif stock.startswith("-") and stock[1:].isdigit():
+        print("Error: Negative numbers are not allowed.")
+
     elif stock.isdigit():
         quantity = int(stock)
         print("Stock quantity:", quantity)
